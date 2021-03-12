ea373b8f78cd6b3e64e024e7a39f0fced5bdef76
管考博 <miwoes@163.com>
2021-03-12 12:58:08 +0800

    增加配置克隆

--- routes/api/site/server/etcd.js
+++ routes/api/site/server/etcd.js
@@ -11,12 +11,58 @@ const {
 } = require("lib/common/util")
 let router = new Router({});
 
-router.post("/", async (ctx) => {
+router.post("/clone", async ctx => {
+    if (!ctx.rbody._siteId) throw new RequiredArgsGeneralityError("_siteId")
+    if (!ctx.rbody.envId) throw new RequiredArgsGeneralityError("envId")
+    if (!ctx.rbody.targetEnvId) throw new RequiredArgsGeneralityError("targetEnvId")
+
+    let result = await mongo.run("server", async db => {
+        let siteServices = await db.collection("siteService").find({
+            siteId: ctx.rbody._siteId
+        }).toArray();
+        let etcds = await db.collection("etcd").find({
+            siteServiceId: {
+                $in: siteServices.map(ss=>ss._id.toString())
+            },
+            envId: ctx.rbody.envId
+        }).toArray()
+
+        if (etcds && etcds.length > 0) {
+            let exists = await db.collection("etcd").count({
+                siteServiceId: {
+                    $in: siteServices.map(ss=>ss._id.toString())
+                },
+                envId: ctx.rbody.targetEnvId
+            })
+
+            if (exists) throw new InvalidArgsGeneralityError("目标环境配置已存在")
+            let env = await db.collection("env").findOne({
+                _id: objectId(ctx.rbody.targetEnvId)
+            })
+
+            let now =  Date.now() / 1000
+            return await db.collection("etcd").insertMany(etcds.map(etcd=>{
+                delete etcd._id
+                return {
+                    ...etcd,
+                    createdAt: now,
+                    updatedAt: now,
+                    sequence: now,
+                    envId: ctx.rbody.targetEnvId,
+                    label: etcd.label.split("-").slice(0, -1).concat([env.label]).join("-")
+                }
+            }))
+        } else {
+            throw new InvalidArgsGeneralityError("原环境配置不存在")
+        }
+    })
+
+    ctx.sbody = result && result.insertedIds
 })
 
-router.put("/:id", async(ctx)=> {
-})
-router.delete("/:id", async(ctx)=> {
-})
+router.post("/", async (ctx) => {})
+
+router.put("/:id", async (ctx) => {})
+router.delete("/:id", async (ctx) => {})
 
 module.exports = router
--- test.js
+++ test.js
@@ -1,2 +1,42 @@
-require("lib/global")
+const json = require('./payload.json')
 
+require("lib/global")
+const mongo = require("lib/service/mongo")
+const objectId = require('mongodb').ObjectId;
+const fs = require("fs");
+const path = require("path");
+const {
+    client
+} = require("application")
+
+
+let run = async () => {
+
+    let bulkData = json.map(data=>{
+        return {
+            label: data.name,
+            name: data.name,
+            category: "business", 
+            description: data.desc,
+            type: data.type,
+            auth: data.auth,
+            image: data.image
+        }
+    })
+
+    await mongo.run("server", async db=> {
+        await db.collection("service").insertMany(bulkData)
+        let services = await db.collection("service").find().toArray()
+        let versions = []
+        
+        services.forEach(service=>{
+            let data = json.find(d=>d.name==service.name)
+            versions = versions.concat(data.versions.map(v=>({name:v,serviceId:service._id.toString()})))
+        })
+        await db.collection("version").insertMany(versions)
+    })
+
+    return "ok"
+}
+
+run().then(console.log).catch(console.error)
